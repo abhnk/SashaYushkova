4f08cdb0a86df537ba1be9fcbdcfbf32a5361c83
abhnk <abhnk1997@gmail.com>
2023-06-06 21:06:16 +0300

    Lesson 17

--- src/test/java/hw12/LoginTest.java
+++ src/test/java/hw12/LoginTest.java
@@ -1,50 +1,48 @@
 package hw12;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.security.Key;
-
-import static org.testng.Assert.assertTrue;
-
 public class LoginTest {
-    @Test
-    public void loginTestPositive() {
-        WebDriverManager.chromedriver().setup();
+    WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/login");
+    }
 
+    @Test
+    public void loginTestPositive() {
         WebElement inputUsername = driver.findElement(By.cssSelector("[name='username']"));
         inputUsername.sendKeys("tomsmith");
         WebElement inputPassword = driver.findElement(By.cssSelector("[name='password']"));
         inputPassword.sendKeys("SuperSecretPassword!" + Keys.ENTER);
         WebElement successMessage = driver.findElement(By.cssSelector(".flash.success"));
-        successMessage.isDisplayed();
-
-        driver.quit();
+        assert successMessage.isDisplayed();
     }
-        @Test
-        public void loginTestNegative() {
-            WebDriverManager.chromedriver().setup();
 
-            WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("https://the-internet.herokuapp.com/login");
+    @Test
+    public void loginTestNegative() {
+        WebElement inputUsername = driver.findElement(By.cssSelector("[name='username']"));
+        inputUsername.sendKeys("abhnk@i.ua");
+        WebElement inputPassword = driver.findElement(By.cssSelector("[name='password']"));
+        inputPassword.sendKeys("SuperSecretPassword!" + Keys.ENTER);
+        WebElement errorMessage = driver.findElement(By.cssSelector(".flash.error"));
+        assert errorMessage.isDisplayed();
+    }
 
-            WebElement inputUsername = driver.findElement(By.cssSelector("[name='username']"));
-            inputUsername.sendKeys("abhnk@i.ua");
-            WebElement inputPassword = driver.findElement(By.cssSelector("[name='password']"));
-            inputPassword.sendKeys("SuperSecretPassword!" + Keys.ENTER);
-            WebElement errorMessage = driver.findElement(By.cssSelector(".flash.error"));
-            errorMessage.isDisplayed();
-
-            driver.quit();
-
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
